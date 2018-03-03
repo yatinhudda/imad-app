63bc63e5b8ed44b891f006e3410e5cf5a780a938
yatinhudda <36735635+yatinhudda@users.noreply.github.com>
2018-03-03 07:06:10 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ var marginLeft=0;
 
 function moveRight(){
     marginLeft=marginLeft+10;
-    img.style.marginLeft=mardinLeft+'px';
+    img.style.marginLeft=marginLeft+'px';
 }
 
 img.onclick=function(){
