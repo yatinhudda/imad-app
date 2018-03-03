b41948cbb589f4865dad2f62b5574b3189178cce
yatinhudda <36735635+yatinhudda@users.noreply.github.com>
2018-03-03 05:49:45 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName',function(req,res){
     //articleName==article-one
     //articles[articleName]=={} content object for article one
-    var articleName=req.params.articeName;
+    var articleName=req.params.articleName;
    res.send(createTemplate(articles[articleName])); 
 });
 
