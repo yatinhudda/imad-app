9d415f119b66f7631839202d0da2a7342f72b257
yatinhudda <36735635+yatinhudda@users.noreply.github.com>
2018-03-03 03:59:19 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ function createTemplate(data){
     var title=data.title;
     var date=data.date;
     var heading=data.heading;
-    var ccontent=data.content;
+    var content=data.content;
 
 var htmlTemplate=`
     <html>
