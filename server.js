6c0f8bf15a9cf797d83b5ce5c07c6c9317113653
yatinhudda <36735635+yatinhudda@users.noreply.github.com>
2018-03-03 04:24:22 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,20 +6,38 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var articleOne={
- title: 'Article one | Yatin hudda',
- heading: 'Article one',
- date: 'mar 1 2018',
- content: `  <p>
-                    This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-                </p>
-                 <p>
-                    This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-                </p>
-                 <p>
-                    This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-                </p> `
- 
+var articles={
+   'article-one':{
+title: 'Article one | Yatin hudda',
+heading: 'Article one',
+date: 'mar 1 2018',
+content: `  <p>
+            This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+        </p>
+         <p>
+            This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+        </p>
+         <p>
+            This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+        </p> `
+
+ },
+   'article-two':{title: 'Article two | Yatin hudda',
+          heading: 'Article two',
+          date: 'mar 3 2018',
+          content: `  <p>
+                           This is the content for my second article....
+                       </p>
+         `
+
+},
+   'article-three':{title: 'Article three | Yatin hudda',
+          heading: 'Article three',
+          date: 'mar 3 2018',
+          content: `  <p>
+                           This is the content for my third article....
+                       </p>
+         `}
 };
 
 function createTemplate(data){
@@ -69,8 +87,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res){
-   res.send(createTemplate(articleOne)); 
+app.get('/:articleName',function(req,res){
+    //articleName==article-one
+    vaar articleName=req.params.articeName
+   res.send(createTemplate(articles[articleName])); 
 });
 
 app.get('/article-two',function(req,res){
