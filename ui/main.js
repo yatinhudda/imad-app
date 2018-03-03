adc35542cb3e433f60700e97b91e79ae616b07b1
yatinhudda <36735635+yatinhudda@users.noreply.github.com>
2018-03-03 06:55:11 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 
 var element=document.getElementById('main-text');
 element.innerHTML='new value';
+
+
+var img=document.getElementById('img');
+
+img.onclick=function(){
+    img.style.marginLeft='100px';  
+};
