fe514ce35f9e35082874b623f6b3ddc72a792a51
yatinhudda <36735635+yatinhudda@users.noreply.github.com>
2018-03-06 10:51:11 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,6 +98,14 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+var counter=0;
+app.get('/counter',function (req,res){
+   counter=counter+1;
+   res.send(counter.toString());
+});
+
+
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
