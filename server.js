ef22ea92fdcc5896fffdee46ca48415bfe79bfce
yatinhudda <36735635+yatinhudda@users.noreply.github.com>
2018-03-13 11:45:33 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,6 +87,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter',function (req,res){
+   counter = counter + 1;
+   res.send(counter.toString());
+});
+
+
+
 app.get('/:articleName',function(req,res){
     //articleName==article-one
     //articles[articleName]=={} content object for article one
@@ -98,11 +106,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-var counter = 0;
-app.get('/counter',function (req,res){
-   counter = counter + 1;
-   res.send(counter.toString());
-});
+
 
 
 
